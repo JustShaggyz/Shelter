5deb3eee2b6ada355cf54d3e02ffe025fa3d413e
Pavel Chervenyashki <pavel.chervenyashki@gmail.com>
2023-05-29 15:02:19 +0300

    Error handling fix

--- src/main/java/com/shelter/config/auth/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/shelter/config/auth/JwtAuthenticationEntryPoint.java
@@ -17,9 +17,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          AuthenticationException authException) throws IOException {
         if (authException.getMessage().contains("ExpiredJwtException")) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token expired");
-        } else {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-        }
+        } 
     }
 }
 
